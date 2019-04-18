ee6c6f0ff08c95d27c6bf3202da7e1be4bbfcb45
molo923 <fauzan_hikmah@yahoo.com>
2019-04-18 18:33:30 +0700

    update

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -20,16 +20,16 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
-            <a class="navbar-brand" href=<?= base_url(); ?>>Telkom University</a>
+            <a class="navbar-brand" href=<?= base_url(); ?>>Smash</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-item nav-link" href="<?= base_url(); ?>">Home</a>
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>mahasiswa">Mahasiswa</a>
-                    <a class="nav-item nav-link" href="#">About</a>
-
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>history">History</a>
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>help">Help</a>
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>profile">Profile</a>
                 </div>
             </div>
         </div>
