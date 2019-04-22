3ee14305830cdc9a647e4c3327345a38a27cd4e7
Sagittifolia <47243258+Sagittifolia@users.noreply.github.com>
2019-04-22 22:57:14 +0700

    Login And LoginAssets

---
+++ application/controllers/Login.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+  public function __construct()
+  {
+    parent::__construct();
+
+
+  }
+
+	public function index()
+	{
+    $data['judul'] = 'Login';
+		$this->load->view('templates/header', $data);
+    $this->load->view('Login/LoginPage');
+    $this->load->view('templates/footer');
+	}
+}
