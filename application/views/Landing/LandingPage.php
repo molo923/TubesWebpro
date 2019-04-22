10a05439fb6ef8b6303e6c1f7d3d07bcfe16fe36
Sagittifolia <47243258+Sagittifolia@users.noreply.github.com>
2019-04-22 20:39:16 +0700

    Reupload LandingPage

---
+++ application/views/Landing/LandingPage.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Landing</title>
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+
+  <!-- Bootstrap core CSS -->
+  <link href="<?php echo base_url('assets/Landing/css/bootstrap.min.css')?>" rel="stylesheet">
+
+  <!-- Material Design Bootstrap -->
+  <link href="<?php echo base_url('assets/Landing/css/mdb.min.css')?>" rel="stylesheet">
+
+  <!-- Your custom styles (optional) -->
+  <link href="<?php echo base_url('assets/Landing/css/style.css')?>" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Start your project here-->
+
+<!--Main Navigation-->
+<header>
+
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark unique-color-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href=<?= base_url(); ?>>Smash</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>">Registration</a>
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+<!--/.Navbar-->
+
+<!--Mask-->
+<div id="intro" class="view">
+
+    <div class="mask rgba-black-strong">
+
+        <div class="container-fluid d-flex align-items-center justify-content-center h-100">
+
+            <div class="row d-flex justify-content-center text-center">
+
+                <div class="col-md-10">
+
+                    <!-- Heading -->
+                    <h2 class="display-4 font-weight-bold white-text pt-5 mb-2">SMASH</h2>
+
+                    <!-- Divider -->
+                    <hr class="hr-light">
+
+                    <!-- Description -->
+                    <h4 class="white-text my-4">Merupakan sebuah web yang dapat membantu masyarakat dalam mengelola sampah.</h4>
+                    <button type="button" class="btn btn-outline-white">Read more<i class="fas fa-book ml-2"></i></button>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+<!--/.Mask-->
+
+</header>
+<!--Main Navigation-->
+
+<!--Main layout-->
+<main>
+<main class="mt-5">
+    <div class="container">
+
+        <!--Section: Best Features-->
+        <section id="best-features" class="text-center">
+
+            <!-- Heading -->
+            <h2 class="mb-5 font-weight-bold">Smash Features</h2>
+
+            <!--Grid row-->
+            <div class="row d-flex justify-content-center mb-4">
+
+            <!--Grid row-->
+            <div class="row">
+
+                <!--Grid column-->
+                <div class="col-md-4 mb-1">
+                    <i class="fas fa-address-card fa-3x"></i>
+                    <h4 class="my-4 font-weight-bold">Daftarnya Mudah</h4>
+                    <p>Mau daftar ke SMASH?, cuma butuh identitas diri. Datamu dijamin aman.</p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-4 mb-1">
+                    <i class="fas fa-comment-dollar fa-3x"></i>
+                    <h4 class="my-4 font-weight-bold">Dapet DUIT</h4>
+                    <p>Daripada sampahmu nganggur dan menumpuk dirumah, mending kamu jual aja pakai SMASH.</p>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-4 mb-1">
+                    <i class="fas fa-bicycle fa-3x"></i>
+                    <h4 class="my-4 font-weight-bold">Layanan Jemput</h4>
+                    <p>Males ngebuang sampah yang ada dirumah?, tenang aja, SMASH ada layanan jemputnya..</p>
+                </div>
+                <!--Grid column-->
+
+            </div>
+            <!--Grid row-->
+
+    </div>
+</main>
+</main>
+<!--Main layout-->
+
+<!-- Footer -->
+<footer class="page-footer font-small blue">
+<footer class="page-footer unique-color-dark">
+    <!-- Social buttons -->
+    <div class="primary-color">
+        <div class="container">
+            <!--Grid row-->
+            <div class="row py-4 d-flex align-items-center">
+
+                <!--Grid column-->
+                <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+                    <h6 class="mb-0 white-text">Get connected with us on social networks!</h6>
+                </div>
+                <!--Grid column-->
+
+                <!--Grid column-->
+                <div class="col-md-6 col-lg-7 text-center text-md-right">
+                    <!--Facebook-->
+                    <a class="fb-ic ml-0">
+                        <i class="fab fa-facebook-f white-text mr-4"> </i>
+                    </a>
+                    <!--Twitter-->
+                    <a class="tw-ic">
+                        <i class="fab fa-twitter white-text mr-4"> </i>
+                    </a>
+                    <!--Google +-->
+                    <a class="gplus-ic">
+                        <i class="fab fa-google-plus-g white-text mr-4"> </i>
+                    </a>
+                    <!--Linkedin-->
+                    <a class="li-ic">
+                        <i class="fab fa-linkedin-in white-text mr-4"> </i>
+                    </a>
+                    <!--Instagram-->
+                    <a class="ins-ic">
+                        <i class="fab fa-instagram white-text mr-lg-4"> </i>
+                    </a>
+                </div>
+                <!--Grid column-->
+            </div>
+            <!--Grid row-->
+        </div>
+    </div>
+    <!-- Social buttons -->
+
+  <!--Footer Links-->
+  <div class="container mt-5 mb-4 text-center text-md-left">
+      <div class="row mt-3">
+
+          <!--First column-->
+          <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
+              <h6 class="text-uppercase font-weight-bold">
+                  <strong>Company name</strong>
+              </h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+                  amet, consectetur adipisicing elit.</p>
+          </div>
+          <!--/.First column-->
+
+          <!--Second column-->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 class="text-uppercase font-weight-bold">
+                  <strong>Products</strong>
+              </h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>
+                  <a href="#!">MDBootstrap</a>
+              </p>
+              <p>
+                  <a href="#!">MDWordPress</a>
+              </p>
+              <p>
+                  <a href="#!">BrandFlow</a>
+              </p>
+              <p>
+                  <a href="#!">Bootstrap Angular</a>
+              </p>
+          </div>
+          <!--/.Second column-->
+
+          <!--Third column-->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+              <h6 class="text-uppercase font-weight-bold">
+                  <strong>Useful links</strong>
+              </h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>
+                  <a href="#!">Your Account</a>
+              </p>
+              <p>
+                  <a href="#!">Become an Affiliate</a>
+              </p>
+              <p>
+                  <a href="#!">Shipping Rates</a>
+              </p>
+              <p>
+                  <a href="#!">Help</a>
+              </p>
+          </div>
+          <!--/.Third column-->
+
+          <!--Fourth column-->
+          <div class="col-md-4 col-lg-3 col-xl-3">
+              <h6 class="text-uppercase font-weight-bold">
+                  <strong>Contact</strong>
+              </h6>
+              <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+              <p>
+                  <i class="fas fa-home  mr-3"></i> New York, NY 10012, US</p>
+              <p>
+                  <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+              <p>
+                  <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+              <p>
+                  <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          </div>
+          <!--/.Fourth column-->
+      </div>
+  </div>
+  <!--/.Footer Links-->
+  </footer>
+<!--Footer-->
+
+  <!-- /Start your project here-->
+
+  <!-- SCRIPTS -->
+  <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+</body>
+
+</html>
