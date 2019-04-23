5337391e27d13750ce75975deab62859a6bdbcc1
Sagittifolia <47243258+Sagittifolia@users.noreply.github.com>
2019-04-23 13:54:19 +0700

    Login page (update)

--- application/views/Login/LoginPage.php
+++ application/views/Login/LoginPage.php
@@ -1,63 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title>Landing</title>
-
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
-
-  <!-- Bootstrap core CSS -->
-  <link href="<?php echo base_url('assets/Landing/css/bootstrap.min.css')?>" rel="stylesheet">
-
-  <!-- Material Design Bootstrap -->
-  <link href="<?php echo base_url('assets/Landing/css/mdb.min.css')?>" rel="stylesheet">
-
-  <!-- Your custom styles (optional) -->
-  <link href="<?php echo base_url('assets/Landing/css/style.css')?>" rel="stylesheet">
-
-</head>
-
+<title>Login Form Design</title>
+<link href="<?php echo base_url('assets/Login/style.css')?>" rel="stylesheet">
 <body>
+    <div class="loginbox">
+        <h1>LOGIN</h1>
+        <form>
+            <p>Username</p>
+            <input type="text" name="" placeholder="Masukkan Username">
+            <p>Password</p>
+            <input type="password" name="" placeholder="Masukkan Password">
+            <input type="submit" name="" value="Login">
+            <a href="#">Lost your password?</a><br>
+            <a href="#">Don't have an account?</a>
+        </form>
 
-  <!-- Start your project here-->
+    </div>
 
-<!--Main Navigation-->
-<header>
-
-<!--Navbar-->
-<nav class="navbar navbar-expand-lg navbar-light primary-color bg-light fixed-top">
-        <div class="container">
-            <a class="navbar-brand" href=<?= base_url(); ?>>Smash</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>">Home</a>
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>history">History</a>
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>help">Help</a>
-                    <a class="nav-item nav-link" href="<?= base_url(); ?>profile">Profile</a>
-                </div>
-            </div>
-        </div>
-    </nav>
-<!--/.Navbar-->
-</header>
-
-
-  <!-- SCRIPTS -->
-  <!-- JQuery -->
-  <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-  <!-- Bootstrap tooltips -->
-  <script type="text/javascript" src="js/popper.min.js"></script>
-  <!-- Bootstrap core JavaScript -->
-  <script type="text/javascript" src="js/bootstrap.min.js"></script>
-  <!-- MDB core JavaScript -->
-  <script type="text/javascript" src="js/mdb.min.js"></script>
 </body>
-
+</head>
 </html>
