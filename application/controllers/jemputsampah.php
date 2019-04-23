dcbc9c2dd9761e57dc1a402432b18a73ae27d162
agung gumelar <agung.gumelar67@gmail.com>
2019-04-23 18:16:15 +0700

    update jemput sampah

--- application/controllers/jemputsampah.php
+++ application/controllers/jemputsampah.php
@@ -13,18 +13,9 @@ class jemputsampah extends CI_Controller{
 		$data['judul'] = 'Home';
 		$this->load->view('templates/header', $data);
 		$this->load->view('templates/footer');
+		$this->load->view('templates/searchbar');
 
-		$this->load->library('googlemaps');
-				$config=array();
-				$config['center']="37.4419, -122.1419";
-				$config['zoom']=17;
-				$config['map_height']="400px";
-				$this->googlemaps->initialize($config);
-				$marker=array();
-				$marker['position']="37.4419, -122.1419";
-				$this->googlemaps->add_marker($marker);
-				$data['map']=$this->googlemaps->create_map();
-		$this->load->view('home/index',$data);
+
 	}
 
 }
