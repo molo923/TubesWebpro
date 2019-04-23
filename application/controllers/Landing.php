0c162c9162e22df07bf680b52c397537d071997d
molo923 <fauzan_hikmah@yahoo.com>
2019-04-23 22:28:23 +0700

    (update)

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -20,8 +20,6 @@ class landing extends CI_Controller {
 	 */
 	public function index()
 	{
-		$data['judul'] = 'Landing Page';
-		$this->load->view('templates/header_Landing', $data);
 		$this->load->view('Landing/LandingPage');
 	}
 }
--- application/views/Landing/LandingPage.php
+++ application/views/Landing/LandingPage.php
@@ -28,6 +28,23 @@
 <!--Main Navigation-->
 <header>
 
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-dark unique-color-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href=<?= base_url(); ?>>Smash</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="<?= base_url('Users/'); ?>register">Registration</a>
+                    <a class="nav-item nav-link" href="<?= base_url(); ?>Login">Login</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+<!--/.Navbar-->
+
 <!--Mask-->
 <div id="intro" class="view">
 
