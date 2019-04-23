db1d55b64c63885817707728d3b2b474b991c070
Rivaldo Ludovicus Sembiring <36322760+rludovicus@users.noreply.github.com>
2019-04-23 20:28:42 +0700

    FAUZAN AGUNG CEK HOME
    Cek bagian index.php udah aku kasih komen nama kalian

---
+++ application/views/home/index.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>HOME</title>
+        <link href="<?php echo base_url('assets/css/style.css')?>" rel="stylesheet">
+        <!-- Bootstrap CSS File -->
+            <link href="<?php echo base_url('assets/lib/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+        <!-- Libraries CSS Files -->
+            <link href="<?php echo base_url('assets/lib/animate/animate.min.css')?>" rel="stylesheet">
+            <link href="<?php echo base_url('assets/lib/font-awesome/css/font-awesome.min.css')?>" rel="stylesheet">
+            <link href="<?php echo base_url('assets/lib/ionicons/css/ionicons.min.css') ?>" rel="stylesheet">
+            <link href="<?php echo base_url('assets/lib/magnific-popup/magnific-popup.css') ?>" rel="stylesheet">
+    </head>
+    <body>
+        <section id="intro">
+        <div class="intro-text">
+            <h2>Selamat Datang di BankSampah.id</h2>
+            <p>Collect.Call.Earn</p>
+            <!--=================
+                UNTUK AGUNG
+            =====================-->
+            <a href="<?php echo base_url() ?>" class="btn-get-started scrollto">Jemput Sampah</a>
+            <!--=================
+                UNTUK FAUZAN
+            =====================-->
+            <a href="<?php echo base_url() ?>timbulan" class="btn-get-started scrollto">Laporkan Timbulan Sampah</a>
+        </div>
+        </section><!-- #intro -->
+
+  <main id="main">
+    </body>
+</html>
